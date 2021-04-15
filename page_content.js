e7f0e30ca11c40304437ea512145cbbb2d0edcb7
Anthony Zuniga <azuniga6290@gmail.com>
2021-04-14 20:49:38 -0400

    Fixed navbar overlap, styles.css

--- page_content.js
+++ page_content.js
@@ -1,51 +1,45 @@
-$("#blog").click(function(){
-    localStorage['current_page']="Blog";
-    print_page();
-  }
-);
-$("#about").click(function(){
-    localStorage['current_page']="About";
-    print_page();
-  }
-);
-$("#projects").click(function(){
-    localStorage['current_page']="Projects";
-    print_page();
-  }
-);
+$("#blog").click(function() {
+  localStorage['current_page'] = "Blog";
+  print_page();
+});
+$("#about").click(function() {
+  localStorage['current_page'] = "About";
+  print_page();
+});
+$("#projects").click(function() {
+  localStorage['current_page'] = "Projects";
+  print_page();
+});
 
-function print_page()
-{
+function print_page() {
   $("#navbarNavDropdown").collapse('hide');
   $("nav").find(".active").removeClass("active");
+  switch (localStorage['current_page']) {
+    case "Blog":
+      print_blog();
+      $("#blog").addClass("active");
+      break;
+    case "About":
+      print_about();
+      $("#about").addClass("active");
+      break;
+    case "Projects":
+      print_projects();
+      $("#projects").addClass("active");
+      break;
+    default:
+      localStorage['current_page'] = "Blog";
+      $("#blog").addClass("active");
+      print_blog();
+  }
   $("#content_header").hide().html(`
-    <div class="jumbotron bg-dark text-white" style="opacity:90%">
-        <h1 class="row justify-content-center">`+localStorage['current_page']+`</h1>
+    <div class="jumbotron bg-dark text-white">
+        <h1 class="row justify-content-center">` + localStorage['current_page'] + `</h1>
     </div>
   `).fadeIn();
-  switch(localStorage['current_page'])
-  {
-      case "Blog":
-        print_blog();
-        $("#blog").addClass("active");
-      break;
-      case "About":
-        print_about();
-        $("#about").addClass("active");
-      break;
-      case "Projects":
-        print_projects();
-        $("#projects").addClass("active");
-      break;
-      default:
-        localStorage['current_page']="Blog";
-        $("#blog").addClass("active");
-        print_blog();
-  }
 }
 
-function print_blog()
-{
+function print_blog() {
   $(function() {
     $("#content").hide().html(`
       <div class="card bg-secondary text-white">
@@ -65,8 +59,7 @@ function print_blog()
   });
 }
 
-function print_about()
-{
+function print_about() {
   $(function() {
     $("#content").hide().html(`
       <div class="card bg-secondary text-white">
@@ -84,8 +77,7 @@ function print_about()
   });
 }
 
-function print_projects()
-{
+function print_projects() {
   $(function() {
     $("#content").hide().html(`
       <div class="card bg-secondary text-white">
